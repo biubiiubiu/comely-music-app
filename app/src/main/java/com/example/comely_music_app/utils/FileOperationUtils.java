85f4619c7fb5f8301c5656291ab612888f6c70d6
zt <3141616128@qq.com>
2022-04-18 10:50:49 +0800

    修改了file工具类

--- app/src/main/java/com/example/comely_music_app/utils/FileOperationUtils.java
+++ app/src/main/java/com/example/comely_music_app/utils/FileOperationUtils.java
@@ -3,7 +3,6 @@ package com.example.comely_music_app.utils;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.util.Log;
-
 import androidx.core.app.ActivityCompat;
 
 import java.io.File;
@@ -13,6 +12,14 @@ import java.io.IOException;
 public class FileOperationUtils {
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static final String[] PERMISSIONS_STORAGE = {"android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+//    List imagePaths = null;
+//    List imageNames = null;
+//    List<Map<String, Object>> imageListItems;
+//
+//    List musicPaths = null;
+//    List musicNames = null;
+//    List<Map<String, Object>> musicListItems;
 
     public static void verifyStoragePermissions(Activity activity) {
         try {
@@ -39,6 +46,63 @@ public class FileOperationUtils {
         outputStream.flush();
         outputStream.close();
     }
+//
+//    void GetImagesPath() {
+//
+//        imagePaths = new ArrayList();
+//        imageNames = new ArrayList();
+//
+//        Cursor cursor = getContentResolver().query(
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+//        while (cursor.moveToNext()) {
+//            //获取图片的名称
+//            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+//            // 获取图片的绝对路径
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            String path = cursor.getString(column_index);
+//            imagePaths.add(path);
+//            imageNames.add(name);
+//
+//            Log.i("GetImagesPath", "GetImagesPath: name = " + name + "  path = " + path);
+//        }
+//        imageListItems = new ArrayList<>();
+//        for (int i = 0; i < imagePaths.size(); i++) {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("name", imagePaths.get(i));
+//            map.put("desc", imageNames.get(i));
+//            imageListItems.add(map);
+//        }
+//    }
+//
+//    void GetMusicPath() {
+//
+//        musicPaths = new ArrayList();
+//        musicNames = new ArrayList();
+//
+//        Cursor cursor = getContentResolver().query(
+//                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+//        while (cursor.moveToNext()) {
+//            //获取图片的名称
+//            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+//            // 获取图片的绝对路径
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+//            String path = cursor.getString(column_index);
+//
+//            musicPaths.add(path);
+//            musicNames.add(name);
+//            File file = new File(path);
+//            Log.i("GetAudioPath", "GetAudioPath: name=" + name + "  path=" + path + "  size=" + file.length());
+//        }
+//        musicListItems = new ArrayList<>();
+//        for (int i = 0; i < musicPaths.size(); i++) {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("name", musicPaths.get(i));
+//            map.put("desc", musicPaths.get(i));
+//            musicListItems.add(map);
+//        }
+//    }
+
+    /*** ============================================================================================== ***/
 
     private static String getDirectory(String filePath) {
         if (filePath.contains("/")) {
@@ -87,4 +151,46 @@ public class FileOperationUtils {
         }
         return true;
     }
+//
+//    //根据路径获取图片
+//    private Bitmap getImgFromDesc(Activity activity, String path) {
+//        Bitmap bmp = null;
+//        File file = new File(path);
+//        // 动态申请权限
+//        String[] permissions = {
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                Manifest.permission.READ_EXTERNAL_STORAGE,
+//                Manifest.permission.CAMERA};
+//        final int REQUEST_CODE = 10001;
+//
+//        // 版本判断。当手机系统大于 23 时，才有必要去判断权限是否获取
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            // 检查该权限是否已经获取
+//
+//            for (String permission : permissions) {
+//                //  GRANTED---授权  DINIED---拒绝
+//                if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), permission) == PackageManager.PERMISSION_DENIED) {
+//                    ActivityCompat.requestPermissions(activity, permissions, REQUEST_CODE);
+//                }
+//            }
+//        }
+//
+//        boolean permission_readStorage = (ContextCompat.checkSelfPermission(activity.getApplicationContext(),
+//                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+//        boolean permission_camera = (ContextCompat.checkSelfPermission(activity.getApplicationContext(),
+//                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+//        if (file.exists()) {
+//            bmp = BitmapFactory.decodeFile(path);
+//        } else {
+//            Log.d("ImgActivity ", "getImgFromDesc: 该图片不存在！");
+//        }
+//        return bmp;
+//    }
+//
+//    // bitmap转byte[]
+//    private byte[] bitmap2Bytes(Bitmap bmp) {
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        return stream.toByteArray();
+//    }
 }
