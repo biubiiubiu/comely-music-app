bf6700f5ea578ab8fefb820a95d7bfd69699106b
zt <3141616128@qq.com>
2022-04-22 16:12:09 +0800

    暂存

--- app/src/main/java/com/example/comely_music_app/MainActivity.java
+++ app/src/main/java/com/example/comely_music_app/MainActivity.java
@@ -6,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import com.example.comely_music_app.R;
 import com.example.comely_music_app.ui.adapter.PlayingViewListAdapter;
 import com.example.comely_music_app.ui.animation.DepthPageTransformer;
+
+import lombok.ToString;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
         PlayingViewListAdapter adapter = new PlayingViewListAdapter();
         viewPager.setAdapter(adapter);
+
+        View play = findViewById(R.id.play_pause_btn);
+        play.setOnClickListener(v->{
+            Toast.makeText(this, "播放", Toast.LENGTH_SHORT).show();
+        });
+
+        View find = findViewById(R.id.find_btn);
+        find.setOnClickListener(v->{
+            Toast.makeText(this, "发现", Toast.LENGTH_SHORT).show();
+        });
+
+        View my = findViewById(R.id.my_btn);
+        my.setOnClickListener(v->{
+            Toast.makeText(this, "我的", Toast.LENGTH_SHORT).show();
+        });
     }
 }
